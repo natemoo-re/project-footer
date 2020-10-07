f967fd3a72bff254c24e90561d6976333759ec98
Nate Moore <nate@natemoo.re>
2020-10-07 09:07:25 -0500

    feat(serverless): use GitHub colors

--- serverless/components/footer.tsx
+++ serverless/components/footer.tsx
@@ -13,7 +13,7 @@ const Footer = ({ children }: { children: any }) => (
       footer {
         margin: 0;
         padding: 1em 0;
-        color: rgba(0, 0, 0, 0.87);
+        color: #24292e;
         font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
         width: 100%;
         flex: 1 1 100%;
@@ -32,7 +32,7 @@ const Footer = ({ children }: { children: any }) => (
       }
 
       p {
-        color: rgba(0, 0, 0, 0.54);
+        color: #24292e;
         font-size: 1em;
       }
 
@@ -43,7 +43,7 @@ const Footer = ({ children }: { children: any }) => (
       }
 
       footer .icon-heart {
-        fill: #e91e63;
+        fill: #cb2431;
         display: inline-block;
         animation: heartbeat 3s ease-in-out infinite;
       }
--- serverless/components/image.tsx
+++ serverless/components/image.tsx
@@ -4,7 +4,7 @@ const Image = ({ width, height, children }) => {
   return (
     <svg fill="none" width={width} height={height} viewBox={`0 0 ${width} ${height}`} xmlns="http://www.w3.org/2000/svg">
       <foreignObject width={width} height={height}>
-        <div {...{ xmlns: 'http://www.w3.org/1999/xhtml', width, height, style: { display: 'flex', flexDirection: 'column', background: '#ebf1f9' } }}>
+        <div {...{ xmlns: 'http://www.w3.org/1999/xhtml', width, height, style: { display: 'flex', flexDirection: 'column', background: '#f6f8fa' } }}>
           <style>{`
               * {
                 margin: 0;
